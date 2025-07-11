faae20734318f02904981cd220845dc2add8efd7
matheusymm <matheusyuitimoriy@gmail.com>
2025-07-11 19:40:03 -0300

    fix: minikube down script

--- scripts/minikube-down.sh
+++ scripts/minikube-down.sh
@@ -5,7 +5,7 @@ cd frontend/
 kubectl delete -f deployment.yaml; kubectl delete -f service.yaml;  
 
 cd ../backend/
-kubectl delete -f deployment.yaml; kubectl delete -f service.yaml; kubectl delete --f secret.yaml;
+kubectl delete -f deployment.yaml; kubectl delete -f service.yaml; kubectl delete -f secret.yaml;
 
 cd ../postgresql/
 kubectl delete -f deployment.yaml; kubectl delete -f service.yaml; kubectl delete -f configmap.yaml; kubectl delete -f secret.yaml; kubectl delete -f pvc.yaml; kubectl delete pv postgresql-pv-volume
